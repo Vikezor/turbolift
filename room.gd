f53c3a93c2659ce601f4f368056de9379bf61e83
Viktor Mitts <vikezor@disroot.org>
2024-11-08 22:17:26 +0200

    add a square creator (it instantiates one square on each click)

---
+++ room.gd
@@ -0,0 +1,15 @@
+extends Polygon2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_room_creator_input_event(viewport, event, shape_idx):
+	pass
