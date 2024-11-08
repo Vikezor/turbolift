851ccdf2c83a9124e9a529e88c982c9a572fe319
Viktor Mitts <vikezor@disroot.org>
2024-11-08 22:45:56 +0200

    add some very janky dragging

--- room.gd
+++ room.gd
@@ -1,5 +1,7 @@
-extends Polygon2D
+extends Area2D
 
+var pressed = false
+var mouseOffset = Vector2(0,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,5 +13,12 @@ func _process(delta):
 	pass
 
 
-func _on_room_creator_input_event(viewport, event, shape_idx):
-	pass
+func _on_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.pressed and not pressed:
+			mouseOffset = get_local_mouse_position()
+			pressed = true
+		else:
+			pressed = false
+	elif event is InputEventMouseMotion and pressed:
+		position = get_global_mouse_position() - mouseOffset
