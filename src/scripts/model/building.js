7893b8de675a45ae102033a2b8b1cd970b90d5f0
Viktor Mitts <vikezor@disroot.org>
2024-11-10 02:31:08 +0200

    add a method to add a floor

--- src/scripts/model/building.js
+++ src/scripts/model/building.js
@@ -9,4 +9,8 @@ export class Building {
         this.floors = this.floors.slice(0,1);
         this.floors[0].reset();
     }
+
+    addFloor(index = this.floors.length) {
+        this.floors.splice(index, 0, new Floorplan());
+    }
 }
